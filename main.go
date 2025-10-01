c6cb2a17ed805d4bbefbae5a72778fcb44210f1d
cadimodev <cadimodev@gmail.com>
2025-10-01 10:34:54 +0200

    Added handler for readiness - added assets folder

--- main.go
+++ main.go
@@ -9,16 +9,27 @@ import (
 func main() {
 	fmt.Println("Welcome to chirpy!")
 
-	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	const filepathRoot = "."
+	const port = "8080"
 
-	server := http.Server{
-		Handler: serveMux,
-		Addr:    ":8080",
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+
+	server := &http.Server{
+		Handler: mux,
+		Addr:    ":" + port,
 	}
 
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
